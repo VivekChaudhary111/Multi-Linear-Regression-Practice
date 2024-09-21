89f2e34ccc4e452d9b1c605fc3bb65f0f6efb609
Vivek Chaudhary <vivek2020singh2020@gmail.com>
2024-09-21 11:21:08 +0530

    this is due to changes

--- Aircraft_fuel_prediction_web_app/app.py
+++ Aircraft_fuel_prediction_web_app/app.py
@@ -13,19 +13,20 @@ st.title('Aircraft Fuel Consumption Predictor')
 flight_distance =st.number_input('Flight Distance (km)')
 number_of_passengers = st.number_input('Number of Passengers')
 flight_duration =st.number_input('Flight Duration (hours)')
-aircraft_type = st.selectbox('Aircraft Type', ['Type 1','Type 2','Type 3'])
+aircraft_type = st.selectbox('Aircraft Type', ['light jets','midsize jets','ultra long range jets'])
 
 # Dataframe
 input_data = pd.DataFrame({
     'Flight_Distance':[flight_distance],
     'Number_of_Passengers':[number_of_passengers],
     'Flight_Duration':[flight_duration],
-    'Aircraft_Type_Type1':[1 if aircraft_type == 'Type1' else 0],
-    'Aircraft_Type_Type2':[1 if aircraft_type == 'Type2' else 0],
-    'Aircraft_Type_Type3':[1 if aircraft_type == 'Type3' else 0]
+    'Aircraft_Type_Type1':[1 if aircraft_type == 'light jets' else 0],
+    'Aircraft_Type_Type2':[1 if aircraft_type == 'midsize jets' else 0],
+    'Aircraft_Type_Type3':[1 if aircraft_type == 'ultra long range jets' else 0]
 })
 
 # Prediction
 if st.button('Predict'):
     fuel_consumption = model.predict(input_data)
-    st.write(fuel_consumption,'')
+    st.header('Fuel consumption')
+    st.write(fuel_consumption[0],'litres')
